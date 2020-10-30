b3a7b0cee671d0ca4c89a427866277da511b0a1c
DariaVynohradova <60809821+DariaVynohradova@users.noreply.github.com>
2020-10-30 08:46:02 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,6 @@ library(dplyr)
 library(stringr)
 library(tidyverse)
 
-
 ##### 1. Merge training and test data
 train <- read.table("C:/rlabwd/UCI HAR Dataset/train/X_train.txt")
 test <- read.table("C:/rlabwd/UCI HAR Dataset/test/X_test.txt")
@@ -15,11 +14,11 @@ features <- read.table("C:/rlabwd/UCI HAR Dataset/features.txt")
 mean_std <- features[str_detect(features$V2, "mean") | str_detect(features$V2, "std"), 1]
 mean_sdt_df<-total[, mean_std]
 
-##### 3. Use activity names to format activity numbers in data set
+##### 3. Use activity names to format activity numbers in the data set
 train_label <- read.table("C:/rlabwd/UCI HAR Dataset/train/y_train.txt")
 test_label <- read.table("C:/rlabwd/UCI HAR Dataset/test/y_test.txt")
 
-
+##### Convert numeric levels of activities to character ones
 train_label$V1 <- as.factor(train_label$V1)
 levels(train_label$V1) <- c('WALKING', 'WALKING_UPSTAIRS', 'WALKING_DOWNSTAIRS', 
                             'SITTING', 'STANDING', 'LAYING')
@@ -34,7 +33,7 @@ total_label <- rbind.data.frame(train_label, test_label)
 mean_std_name <- as.character(features[str_detect(features$V2, "mean") | str_detect(features$V2, "std"), 2])
 names(mean_sdt_df) <- mean_std_name
 
-##### 5. Create tidy data set with calculated avarage for each activity and subject
+##### 5. Create tidy data set with the calculated average for each activity and subject
 train_subject <- read.table("C:/rlabwd/UCI HAR Dataset/train/subject_train.txt")
 test_subject <- read.table("C:/rlabwd/UCI HAR Dataset/test/subject_test.txt")
 total_subject <- rbind.data.frame(train_subject, test_subject)
